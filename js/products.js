ce43303a0a5b3a44e56a203376f5377f181170c1
Erick-Amado <erickolivera044@gmail.com>
2026-03-13 23:19:23 -0600

    agregue diseño con bootstrap

--- js/products.js
+++ js/products.js
@@ -1,29 +1,39 @@
 fetch("https://fakestoreapi.com/products")
 
-.then(res => res.json())
+.then(res=>res.json())
 
-.then(products => {
+.then(products=>{
 
 let html=""
 
 products.forEach(p=>{
 
 html+=`
-<div>
 
-<h3>${p.title}</h3>
+<div class="col-md-4 mb-4">
 
-<img src="${p.image}" width="100">
+<div class="card h-100">
 
-<p>$${p.price}</p>
+<img src="${p.image}" class="card-img-top" style="height:200px;object-fit:contain">
 
-<button onclick="addCart('${p.title}',${p.price})">
+<div class="card-body">
+
+<h5 class="card-title">${p.title}</h5>
+
+<p class="card-text">$${p.price}</p>
+
+<button class="btn btn-primary" onclick="addCart('${p.title}',${p.price})">
 
 Agregar al carrito
 
 </button>
 
 </div>
+
+</div>
+
+</div>
+
 `
 
 })
@@ -31,15 +41,3 @@ Agregar al carrito
 document.getElementById("products").innerHTML=html
 
 })
-
-function addCart(name,price){
-
-let cart = JSON.parse(localStorage.getItem("cart")) || []
-
-cart.push({name,price})
-
-localStorage.setItem("cart",JSON.stringify(cart))
-
-alert("Producto agregado")
-
-}
